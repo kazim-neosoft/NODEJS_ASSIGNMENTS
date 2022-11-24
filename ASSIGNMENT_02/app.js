a81cff1a8df47f28c81066517814db39bea580b8
kazim-neosoft <kazim.sayed@neosoftmail.com>
2022-11-24 14:30:32 +0530

    Assignment 02

---
+++ ASSIGNMENT_02/app.js
@@ -0,0 +1,75 @@
+const express=require('express');
+const fs=require("fs");
+const app=express()
+const PORT=9999
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use("/static",express.static('public'))
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+
+app.get("/gallery",(req,res)=>{
+    res.render("gallery")
+})
+
+app.get("/service",(req,res)=>{
+    res.render("services")
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+})
+
+app.post("/submitdata",(req,res)=>{
+    const fullName=req.body.name;
+    const email=req.body.email;
+    const phoneNumber=req.body.phnNumber;
+    // Uncomment below code to write data in pug
+//     const data=`
+// tr
+//  td ${fullName}
+//  td ${email}
+//  td ${phoneNumber}
+//  `
+const data=`
+<tr>
+    <td>${fullName}</td>
+    <td>${email}</td>
+    <td>${phoneNumber}</td>
+</tr>
+`
+    
+    if(fs.existsSync('./contactdata/details.pug')){
+        fs.appendFileSync('./contactdata/details.pug',data.toString(),"utf8");
+        
+    }else{
+        fs.writeFileSync('./contactdata/details.pug',data.toString(),"utf8");
+    }
+    res.render("contactDetails")
+})
+
+app.get("/details",(req,res)=>{
+    
+        const data = fs.readFileSync('./contactdata/details.pug');
+        console.log("Data>>>>", data)
+        res.render('contactDetails',{data:data})
+})
+
+app.use("*",(req,res)=>{
+    res.status(404).render("404")
+})
+
+app.listen(PORT,(err)=>{
+    if(err) throw err;
+    console.log(`listening at port ${PORT}`);
+})
