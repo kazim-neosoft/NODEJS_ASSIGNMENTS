93fe9e10dd5661294566d6a7e7f1e45f0737571e
kazim-neosoft <kazim.sayed@neosoftmail.com>
2022-11-25 09:17:56 +0530

    adding documentation

--- ASSIGNMENT_02/app.js
+++ ASSIGNMENT_02/app.js
@@ -3,9 +3,11 @@ const fs=require("fs");
 const app=express()
 const PORT=9999
 
+// setting pug view engine 
 app.set('view engine','pug');
 app.set('views','./views');
 
+// using inbuilt middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use("/static",express.static('public'))
@@ -34,30 +36,36 @@ app.post("/submitdata",(req,res)=>{
     const fullName=req.body.name;
     const email=req.body.email;
     const phoneNumber=req.body.phnNumber;
+    // creating data variable and formatting the table row and table data
     const data=`
 tr
  td ${fullName}
  td ${email}
  td ${phoneNumber}
  `
-
+    // checking if file exist or not
     if(fs.existsSync('./contactdata/details.pug')){
+        // if exist update data
         fs.appendFileSync('./contactdata/details.pug',data.toString(),"utf8");
         
     }else{
+        // if not write new file with name details.pug
         fs.writeFileSync('./contactdata/details.pug',data.toString(),"utf8");
     }
     res.render("contactDetails")
 })
 
+// for rendering contact details
 app.get("/details",(req,res)=>{
     res.render("contactDetails")
 })
 
+// handling unwanted request and render 404 page
 app.use("*",(req,res)=>{
     res.status(404).render("404")
 })
 
+// listening on port 
 app.listen(PORT,(err)=>{
     if(err) throw err;
     console.log(`listening at port ${PORT}`);
